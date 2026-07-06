e7415ce598bd09dd54543392796631b092060d32
Arun KS <arun-ks@users.noreply.github.com>
2026-07-06 17:08:37 +0800

    Remove console log from db.js
    Removed console log for TURSO_DATABASE_URL.

--- api/_lib/db.js
+++ api/_lib/db.js
@@ -4,5 +4,3 @@ export const db = createClient({
   url: process.env.TURSO_DATABASE_URL,
   authToken: process.env.TURSO_AUTH_TOKEN
 });
-
-console.log("TURSO_DATABASE_URL = ", process.env.TURSO_DATABASE_URL);
